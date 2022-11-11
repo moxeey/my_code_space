ca4ecd72e1c38ec721c457a553e8ae6337126025
Muhammad Zubairu <muhdzubairuahmad@gmail.com>
2022-11-11 19:32:00 +0100

    solutions 3: Rotate Image in-place

--- code-signal/solutions.js
+++ code-signal/solutions.js
@@ -27,3 +27,17 @@ const solution1=(s) => {
 
     return unique.size>0? [...unique][unique.size-1]:'_'
 }
+
+// Rotate Image (In-place)
+const solution2=(a) => {
+
+    for(let i=0;i<a.length;i++)
+        for(let j=0;j<i;j++) {
+            let temp=a[i][j]
+            a[i][j]=a[j][i]
+            a[j][i]=temp
+        }
+    for(let i=0;i<a.length;i++)
+        a[i].reverse()
+    return a
+}
